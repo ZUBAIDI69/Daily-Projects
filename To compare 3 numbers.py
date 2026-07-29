6dc6825eec8b2d8ffdb87bd085df42078d59569a
ZUBAIDI69 <nafiz7209@gmail.com>
2026-07-29 22:58:51 +0600

    Add files via upload

---
+++ To compare 3 numbers.py
@@ -0,0 +1,12 @@
+a = int(input("Enter A:"))
+b = int(input("Enter B:"))
+c = int(input("Enter C:"))
+if a > b:
+    if a > c:
+        print(A,"is greater.")
+    else:
+        print(c,"is greater.")
+if b > c :
+    print(b,"is greater.")
+else:
+    print(c,"is greater.")
